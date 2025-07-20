15d44f977af465e48a6f10dbe0fb592dabffaf5c
Muskan Chaudhary <chaudharymuskan330@gmail.com>
2025-07-20 08:53:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init({ duration: 1000, once: true });
+
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
